4e19a9afca3051b1eff38dceff362ae8611288e6
Angelito De Sagun <desagunangelito@gmail.com>
2022-11-03 23:35:53 +0800

    Manual Testing for BMI Calculator

--- bmi_calculator.js
+++ bmi_calculator.js
@@ -1,14 +1,20 @@
 // weight(kg)
 // height(m)
 function calculateBMI(weight, height){
-    return weight / (height ** 2);
+    return Math.round((weight / (height ** 2)) * 100) / 100;
 }
 
 let weight = 90;
 let height = 1.61;
 let bmi = 0;
+const expected = 34.72
 
 bmi = calculateBMI(weight, height);
 
 
 console.log(`Your BMI is ${bmi}`);
+
+console.assert(
+    bmi === expected,
+    `Checking result of calculateBMI(90, 161) - Expects: ${expected} Received: ${bmi}`
+);
